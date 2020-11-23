3f11767d34d56e7571be2b686e52dd553312b600
Dr Z <zamore@vt.edu>
2020-11-23 15:09:43 -0700

    for use with head FOV renderings
    used to invert the surface of the mesh such that rays that are sent from inside the head can collide with the3 structure. In the most recent version of the FOV code, this is NOT required (rays cast from outside of the surface)

---
+++ AddInvertedMeshCollider.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Linq;
+using System.Collections;
+// modified from https://forum.unity.com/threads/can-you-invert-a-sphere-or-box-collider.118733/
+
+public class AddInvertedMeshCollider : MonoBehaviour
+{
+	public bool removeExistingColliders = true;
+
+	public void CreateInvertedMeshCollider()
+	{
+		if (removeExistingColliders)
+			RemoveExistingColliders();
+
+		InvertMesh();
+
+		gameObject.AddComponent<MeshCollider>();
+	}
+
+	private void RemoveExistingColliders()
+	{
+		Collider[] colliders = GetComponents<Collider>();
+		for (int i = 0; i < colliders.Length; i++)
+			DestroyImmediate(colliders[i]);
+	}
+
+	private void InvertMesh()
+	{
+		Mesh mesh = GetComponent<MeshFilter>().mesh;
+		mesh.triangles = mesh.triangles.Reverse().ToArray();
+	}
+}
